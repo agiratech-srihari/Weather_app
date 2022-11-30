d55072f1064a3836a03a8f38090369fcfdd9a239
agiratech-srihari <srihari.t@agiratech.com>
2022-11-30 19:17:55 +0530

    working on weather app

---
+++ src/Api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a027c8559amsh4a984e0b6f8f9aep1bcd39jsn80e8b9b5d227',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+
+export const Geo_API_URl = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
